e77c925a322f00804e6217ca25be318e6b17b974
Marine Gapihan <marine22@mit.edu>
2025-03-10 10:17:50 -0400

    hh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 
 <script>
 import { page } from "$app/stores";
+import "../styles/style.css";
+
 
 let pages = [
-	{url: "./", title: "Home"},
-	{url: "projects", title: "Projects"},
-    {url: "contact", title: "Contact"},
-    {url: "CV", title: "Resume"},
+	{url: "/", title: "Home"},
+	{url: "/projects", title: "Projects"},
+    {url: "/contact", title: "Contact"},
+    {url: "/CV", title: "Resume"},
     {url: "https://github.com/marinegapihan2", title: "GitHub" }
 ];
 
@@ -26,42 +28,6 @@ let pages = [
   </a>
   {/each}
 </nav>
-
-  <style>
-
-  nav ul,
-nav li {
-    display: contents;
-}
-
-nav {
-  display: flex;
-  }
-  nav a {
-    flex: 1;
-    text-decoration: none ;
-    color: inherit;
-    text-align: center;
-    padding: 1em;
-    margin-bottom: 3ch ;
-    border-bottom-width: 1px;
-    border-bottom-style: solid;
-    border-color: oklch(50% 10% 200 / 40%);
-  }
-
-  nav a.current {
-    border-bottom: 0.4em solid oklch(50% 10% 200/40%);
-    padding-bottom: calc(10px - 0.4em);
-  }
-
-
-  nav a:hover {
-  border-bottom:0.4em solid var(--color-accent) ;
-  background-color: color-mix(in oklch, var(--color-accent), canvas 85%);
-  }
-
-</style>
-
 
 
 YOLO <slot/>
