3f4e8d10d0f8788230aafd6a9bf8e616a86afc59
Marine Gapihan <marine22@mit.edu>
2025-03-10 09:11:21 -0400

    hh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 
 <script>
+import { page } from "$app/stores";
+
 let pages = [
 	{url: "./", title: "Home"},
 	{url: "projects", title: "Projects"},
@@ -8,15 +10,58 @@ let pages = [
     {url: "https://github.com/marinegapihan2", title: "GitHub" }
 ];
 
-import { page } from "$app/stores";
-
 
 </script>
 
+
 <nav>
+
     {#each pages as p}
-    <a href="{p.url}">{p.title}</a>
-    {/each}
+    <a
+    href={p.url}
+    class:current={$page.route.id === p.url}
+    target={p.url.startsWith("http") ? "_blank" : null}
+  >
+    {p.title}
+  </a>
+  {/each}
 </nav>
+
+  <style>
+
+  nav ul,
+nav li {
+    display: contents;
+}
+
+nav {
+  display: flex;
+  }
+  nav a {
+    flex: 1;
+    text-decoration: none ;
+    color: inherit;
+    text-align: center;
+    padding: 1em;
+    margin-bottom: 3ch ;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-color: oklch(50% 10% 200 / 40%);
+  }
+
+  nav a.current {
+    border-bottom: 0.4em solid oklch(50% 10% 200/40%);
+    padding-bottom: calc(10px - 0.4em);
+  }
+
+
+  nav a:hover {
+  border-bottom:0.4em solid var(--color-accent) ;
+  background-color: color-mix(in oklch, var(--color-accent), canvas 85%);
+  }
+
+</style>
+
+
 
 YOLO <slot/>
