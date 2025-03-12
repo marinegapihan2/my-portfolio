943057c85526f64ee0ad99314baa7dc5aea67581
Marine Gapihan <marine22@mit.edu>
2025-03-12 11:02:47 -0400

    hh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,9 @@ import "../styles/style.css";
 
 let pages = [
 	{url: "/", title: "Home"},
-	{url: "/projects", title: "Projects"},
-    {url: "/contact", title: "Contact"},
-    {url: "/CV", title: "Resume"},
+	{url: "projects", title: "Projects"},
+    {url: "contact", title: "Contact"},
+    {url: "CV", title: "Resume"},
     {url: "https://github.com/marinegapihan2", title: "GitHub" }
 ];
 
