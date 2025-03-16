89c008f8eb328d7a514024103a6a943ad1204c53
Marine Gapihan <marine22@mit.edu>
2025-03-16 14:33:58 -0400

    03/16

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -1,26 +1,42 @@
 <script>
 import * as d3 from 'd3';
+import "../styles/style.css";
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 let arc = arcGenerator({
 	startAngle: 0,
 	endAngle: 2 * Math.PI
 });
-let data = [1, 2, 3, 4, 5, 5];
-let sliceGenerator = d3.pie();
+let data = [
+	{ value: 1, label: "apples" },
+	{ value: 2, label: "oranges" },
+	{ value: 3, label: "mangos" },
+	{ value: 4, label: "pears" },
+	{ value: 5, label: "limes" },
+	{ value: 5, label: "cherries" }
+];
+let sliceGenerator = d3.pie().value(d => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map(d => arcGenerator(d));
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-
 </script>
 
-<svg viewBox="-50 -50 100 100">
-    {#each arcs as arc, index}
-  <path d={ arc } fill={ colors(index) } />
-{/each}
 
-
-</svg>
+<div class="container">
+	<svg viewBox="-50 -50 100 100">
+		{#each arcs as arc, index}
+		<path d={ arc } fill={ colors(index) } />
+	  {/each}
+	</svg>
+	<ul class="legend">
+		{#each data as d, index}
+		<li style="--color: { colors(index) }">
+			<span class="swatch"></span>
+			{d.label} <em>({d.value})</em>
+		</li>
+	{/each}
+	</ul>
+</div>
 
 <style>
     svg {
@@ -30,4 +46,37 @@ let colors = d3.scaleOrdinal(d3.schemeTableau10);
 	overflow: visible;
 }
 
+.swatch {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background-color: var(--color);
+
+}
+
+
+	ul{
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(8em, 1fr));
+		border: 0.5px solid black ;
+		padding: 1%;
+		margin: 1ch;
+
+	}
+
+	li{
+	display: flex;
+	align-items: center;
+	gap: 0.5em;
+	}
+
+.container{
+	display: flex;
+	align-items: center; /* Aligns items vertically */
+    gap: 20px;
+}
+
+.legend{
+	flex: 1;
+}
 </style>
