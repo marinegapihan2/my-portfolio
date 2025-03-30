18656823ec0f426840845f38e8ef0bf1467540f5
Marine Gapihan <marine22@mit.edu>
2025-03-30 14:55:53 -0400

    03/30 second

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
 <script>
   import Project from "$lib/Project.svelte";
   import projects from "$lib/projects.json";
+  import { onMount } from "svelte";
+
+let githubData = null;
+let loading = true;
+let error = null;
+
+onMount(async () => {
+	try {
+		const response = await fetch("https://api.github.com/users/marinegapihan2");
+		githubData = await response.json();
+	} catch (err) {
+		error = err;
+	}
+	loading = false;
+});
+
 </script>
 
 <svelte:head>
@@ -11,29 +27,24 @@
     <p>Marine is a MCP1 student at MIT, originally from France. Not used to the snow, she returned to Cambridge without any snow apparel and fell to the ground two minutes after taking this picture :D </p>
     <img src="images/snow.jpeg" alt= "enjoying the snow from Hayden library">
 
-    {#await fetch("https://api.github.com/users/marinegapihan2")}
+    {#if loading}
     <p>Loading...</p>
-  {:then response}
-    {#await response.json()}
-      <p>Decoding...</p>
-    {:then data}
-      <section>
+{:else if error}
+    <p class="error">Something went wrong: {error.message}</p>
+{:else}
+    <section>
         <h2>My GitHub Stats</h2>
         <dl>
-          <dt>Followers:</dt>
-          <dd>{data.followers}</dd>
-          <dt>Following:</dt>
-          <dd>{data.following}</dd>
-          <dt>Public Repositories:</dt>
-          <dd>{data.public_repos}</dd>
+            <dt>Followers</dt>
+            <dd>{githubData.followers}</dd>
+            <dt>Following</dt>
+            <dd>{githubData.following}</dd>
+            <dt>Public Repositories</dt>
+            <dd>{githubData.public_repos}</dd>
         </dl>
-      </section>
-    {:catch error}
-      <p class="error">Something went wrong: {error.message}</p>
-    {/await}
-  {:catch error}
-    <p class="error">Something went wrong: {error.message}</p>
-  {/await}
+    </section>
+{/if}
+
 
 <br>
     <h2>Latest Projects</h2>
--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -2,6 +2,80 @@
   <title>Meta</title>
 </svelte:head>
 
+<script>
+import * as d3 from "d3";
+import { onMount } from "svelte";
+
+let data = [];
+let commits = [];
+let numFiles = 0;
+let mostWorkDayOfWeek = "";
+
+
+onMount(async () => {
+	data = await d3.csv("/loc.csv", row => ({
+	...row,
+	line: Number(row.line), // or just +row.line
+	depth: Number(row.depth),
+	length: Number(row.length),
+	date: new Date(row.date + "T00:00" + row.timezone),
+	datetime: new Date(row.datetime),
+    file: row.file,
+}));
+numFiles = new Set(data.map(d => d.file)).size;
+const dayOfWeekCount = { Sunday: 0, Monday: 0, Tuesday: 0, Wednesday: 0, Thursday: 0, Friday: 0, Saturday: 0 };
+    data.forEach(d => {
+      const dayOfWeek = d.datetime.toLocaleString('en-US', { weekday: 'long' });
+      dayOfWeekCount[dayOfWeek]++;
+    });
+    mostWorkDayOfWeek = Object.keys(dayOfWeekCount).reduce((a, b) => dayOfWeekCount[a] > dayOfWeekCount[b] ? a : b);
+
+commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
+	let first = lines[0];
+	let {author, date, time, timezone, datetime} = first;
+	let ret = {
+		id: commit,
+		url: "https://github.com/vis-society/lab-7/commit/" + commit,
+		author, date, time, timezone, datetime,
+		hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+		totalLines: lines.length
+	};
+
+	Object.defineProperty(ret, "lines", {
+		value: lines,
+		configurable: true,
+		writable: true,
+		enumerable: false,
+	});
+
+	return ret;
+});
+console.log(commits);
+
+});
+
+
+</script>
+
+
 <h1>META</h1>
 
 <p>This page includes stats about the code of this website</p>
+
+
+<section>
+    <h2>Project Stats</h2>
+    <dl class="stats">
+        <dt>Total <abbr title="Lines of code">LOC</abbr></dt>
+        <dd>{data.length}</dd>
+
+        <dt>Commits</dt>
+        <dd>{commits.length}</dd>
+
+        <dt>Number of Files</dt>
+        <dd>{numFiles}</dd>
+
+        <dt>Most Work Done on</dt>
+        <dd>{mostWorkDayOfWeek}</dd>
+     </dl>
+</section>
