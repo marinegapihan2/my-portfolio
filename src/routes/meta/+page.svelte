08840e9fe7db015e2c14b55749a6426a50bdfd89
Marine Gapihan <marine22@mit.edu>
2025-03-31 12:10:54 -0400

    03/31

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -10,6 +10,20 @@ let data = [];
 let commits = [];
 let numFiles = 0;
 let mostWorkDayOfWeek = "";
+let width = 1000, height = 600;
+let margin = {top: 10, right: 10, bottom: 30, left: 20};
+let usableArea = {
+	top: margin.top,
+	right: width - margin.right,
+	bottom: height - margin.bottom,
+	left: margin.left
+};
+usableArea.width = usableArea.right - usableArea.left;
+usableArea.height = usableArea.bottom - usableArea.top;
+
+let xAxis, yAxis;
+
+let yAxisGridlines;
 
 
 onMount(async () => {
@@ -54,6 +68,35 @@ console.log(commits);
 
 });
 
+$: minDate = d3.min(commits.map(d => d.date));
+$: maxDate = d3.max(commits.map(d => d.date));
+$: maxDatePlusOne = new Date(maxDate);
+$: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+
+$: xScale = d3.scaleTime()
+              .domain([minDate, maxDatePlusOne])
+              .range([usableArea.left, usableArea.right])
+              .nice();
+
+$: yScale = d3.scaleLinear()
+              .domain([24, 0])
+              .range([usableArea.bottom, usableArea.top]);
+
+$: {
+	d3.select(xAxis).call(d3.axisBottom(xScale));
+	d3.select(yAxis).call(d3.axisLeft(yScale));
+	d3.select(yAxis).call(d3.axisLeft(yScale).tickFormat(d => String(d % 24).padStart(2, "0") + ":00"));
+
+}
+
+$: {
+	d3.select(yAxisGridlines).call(
+		d3.axisLeft(yScale)
+		  .tickFormat("")
+		  .tickSize(-usableArea.width)
+	);
+}
+
 
 </script>
 
@@ -79,3 +122,31 @@ console.log(commits);
         <dd>{mostWorkDayOfWeek}</dd>
      </dl>
 </section>
+
+<h2>Commits by time of day</h2>
+<svg viewBox="0 0 {width} {height}">
+	<g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+	<g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+	<g class="gridlines" transform="translate({usableArea.left}, 0)" bind:this={yAxisGridlines} />
+	<g class="dots">
+		{#each commits as commit, index }
+			<circle
+				cx={ xScale(commit.datetime) }
+				cy={ yScale(commit.hourFrac) }
+				r="5"
+				fill="steelblue"
+			/>
+		{/each}
+		</g>
+
+</svg>
+
+<style>
+	svg {
+		overflow: visible;
+	}
+	.gridlines {
+	stroke-opacity: .2;
+}
+
+</style>
